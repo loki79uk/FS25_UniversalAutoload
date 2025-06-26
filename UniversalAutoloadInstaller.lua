6ba9c4cb904e47d8bd9d194ca86b4a6e0625b08f
Loki_79 <76516343+loki79uk@users.noreply.github.com>
2025-06-26 22:49:44 +0100

    Remove active vehicle debug display

--- UniversalAutoloadInstaller.lua
+++ UniversalAutoloadInstaller.lua
@@ -382,24 +382,24 @@ ActivatableObjectsSystem.updateObjects = Utils.overwrittenFunction(ActivatableOb
 function UniversalAutoloadManager:update(dt)
 	
 	if g_currentMission:getIsServer() then
-		local activeVehicles = {}
+		-- local activeVehicles = {}
 		for vehicle, _ in pairs(UniversalAutoload.VEHICLES) do
 			local spec = vehicle and vehicle.spec_universalAutoload
 			local doActivateExtra = spec and spec.autoCollectionMode or vehicle == UniversalAutoload.lastClosestVehicle
 			if doActivateExtra then
 				UniversalAutoload.doUpdate(vehicle, dt)
 				UniversalAutoload.onDraw(vehicle)
-				table.insert(activeVehicles, vehicle)
-			elseif vehicle:getIsActiveForInput(true, true) then
-				table.insert(activeVehicles, vehicle)
+				-- table.insert(activeVehicles, vehicle)
+			-- elseif vehicle:getIsActiveForInput(true, true) then
+				-- table.insert(activeVehicles, vehicle)
 			end
 		end
-		if UniversalAutoload.showDebug and next(activeVehicles) ~= nil then
-			renderText(0.21, 0.925, 0.015, "UAL Active Vehicles:")
-			for i, vehicle in ipairs(activeVehicles) do
-				renderText(0.21, 0.92 - (i * 0.015), 0.012, tostring(vehicle:getFullName()))
-			end
-		end
+		-- if UniversalAutoload.showDebug and next(activeVehicles) ~= nil then
+			-- renderText(0.21, 0.925, 0.015, "UAL Active Vehicles:")
+			-- for i, vehicle in ipairs(activeVehicles) do
+				-- renderText(0.21, 0.92 - (i * 0.015), 0.012, tostring(vehicle:getFullName()))
+			-- end
+		-- end
 	end
 	
 	for id, object in pairs(brokenTensionBeltObjects or {}) do
